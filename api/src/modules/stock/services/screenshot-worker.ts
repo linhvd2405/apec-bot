28c79c980c8d805a0cf2574a40f01e992c3c4e5a
linhvd2405 <vuduylinh24052002@gmail.com>
2023-05-22 16:03:03 +0700

    update project

--- api/src/modules/stock/services/screenshot-worker.ts
+++ api/src/modules/stock/services/screenshot-worker.ts
@@ -1,5 +1,6 @@
+
 import { IStock } from '../interfaces/stock._id.interface';
-import { WebDriver,By, Builder,until } from 'selenium-webdriver';
+import { WebDriver,By, Builder } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
 import * as fs from 'fs';
 import axios from 'axios';
@@ -20,23 +21,29 @@ export class ScreenshotWorker {
         .headless()
         .windowSize({ width: 900, height: 12000 })
         .addArguments('--no-sandbox')
+        .addArguments("--disable-web-security")
+        .addArguments("--allow-running-insecure-content")
+        .addArguments("--ignore-certificate-errors")
+        .addArguments('--disable-dev-shm-usage')
       )
-      .usingServer('http://selenium:4444')
+      .usingServer(process.env.SELENIUM_URL)
       .build();
-      console.log("đã vào selenium"  )
+
 
     try {
-      console.log("đã vào selenium"  )
+      // console.log("\n______IN HERE _____\n");
+      
       const baseUrl = process.env.BASE_URL;
+      // console.log("da vao chrome", `${baseUrl}/${this.stock._id.toString()}`)
       await driver.get(`${baseUrl}/${this.stock._id.toString()}`);
-      // await driver.get(`${baseUrl}`);
-      console.log("đã vào trang :" +`${baseUrl}/${this.stock._id.toString()}` )
       await driver.sleep(3000); // Wait for 3 seconds
-      // const bodyElement = await driver.findElement(By.css('#container > div'));
-      const bodyElement = await driver.wait(until.elementLocated(By.css('body')), 10000);
+      const bodyElement = await driver.findElement(By.css('#container > div'));
+      // console.log("da tim duoc div" )
       const screenshot = await bodyElement.takeScreenshot();
       fs.writeFileSync(`src/content/image/${this.stock.stockCode}.jpg`, screenshot, 'base64');
+      // console.log(`src/content/image/${this.stock.stockCode}.jpg`)
       console.log(`Screenshot taken successfully for ${this.stock.stockCode}`);
+      
 
       const formData = new FormData();
       const imageFile = await fs.promises.readFile(`src/content/image/${this.stock.stockCode}.jpg`);
